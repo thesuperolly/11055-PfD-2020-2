b6d84c26d323312ac0ef0a95c49779c442cfe8b5
ZNB77 <zbendebel@yahoo.com>
2020-08-31 10:28:52 +1000

    commiting week 4 work

---
+++ students/u3201052/wk3/FlowchartPseudocode.js
@@ -0,0 +1,23 @@
+//CREATE Variables [String: Message, DateTime: Noon, DateTime: Evening, Num: BusSchedule]
+
+var Message;
+var Sunrise;
+
+//Message = “Go to school by bus”
+Message = "Go to school by bus";
+//Morning = “8pm”
+//Afternoon = “4pm”
+//BusSchedule = ["hours till class starts"]
+//CurrentTime = [BusSchedule]
+
+//DO WHILE CurrentTime > Morning & < Afternoon
+	//IF BusSchedule >1
+	//  SEND [Message]
+
+//IF CurrentTime > Morning & CurrentTime < Afternoon THEN
+	//IF BusSchedule < 1 THEN 
+	//	DRIVE [Car]
+//ELSE 
+	//END
+
+//END PROGRAM
