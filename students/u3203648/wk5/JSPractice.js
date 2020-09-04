edf1f1de919caf5674793082c058d4c72aa9e164
NextQueenLiz <ebazila@gmail.com>
2020-09-04 13:29:52 +1000

    commit

--- students/u3203648/wk5/JSPractice.js
+++ students/u3203648/wk5/JSPractice.js
@@ -5,3 +5,19 @@
 // LOOP while "asleep"
 // ELSE End
 
+var current_time = 1; //we will start at 1 and count up from there
+var sleep_status = (math.ceil)(math.random() * 10); //this would be replaced with a movement/noise senor
+//for the purposes of this less than 4 is asleep
+var alarm = "beep beep beep"
+//would be replaced with an actual alarm
+var get_up = 9; //
+
+while (current_time >= get_up) {
+
+    if (sleep_status < 4) {
+        document.write(alarm);
+        break;
+    } else {
+
+    }
+}
