27e34493f44d30bbb9d1f4077762286a94a0c722
ZNB77 <zbendebel@yahoo.com>
2020-09-10 01:24:24 +1000

    wk6 first commit

---
+++ students/u3201052/wk6/wk6_ex2_script.js
@@ -0,0 +1,17 @@
+/* Name/ID: Zanya Nadelle Bendebel
+Date: 7 September 2020
+This file is exercise 2 focusing on family names
+*/
+
+// Arrays
+
+//create
+
+var family_names = ["Leo", "Leone", "Nira", "Niro", "Shan", "Shannon"];
+
+document.write("Names in family array <br/>");
+for(let i = 0; i < family_names.length; i++;) {
+
+    document.write(family_names[i] + "<br/>")
+
+}
